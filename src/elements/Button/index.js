e24b00b14672f7870ee703d294df6ea9e1616ae0
Septian Cahya <cahyaseptian39@gmail.com>
2022-09-14 12:01:05 +0700

    finis component part/mostpicked + categories

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -11,7 +11,7 @@ export default function Button(props) {
   if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
-    if (props.onClick) props.onCLick();
+    if (props.onClick) props.onClick();
   };
 
   if (props.isDisabled || props.isLoading) {
@@ -62,6 +62,7 @@ export default function Button(props) {
       href={props.href}
       className={className.join(" ")}
       style={props.style}
+      onClick={onClick}
     >
       {props.children}
     </button>
