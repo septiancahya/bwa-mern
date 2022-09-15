433c8c0d1a6eba42d4684d538ba5de7b36222aa1
Septian Cahya <cahyaseptian39@gmail.com>
2022-09-15 14:49:13 +0700

    adding react-reveal animation each component in landingpage

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "elements/Button";
 import BrandIcon from "parts/IconText";
+import Fade from "react-reveal/Fade";
 
 export default function Header(props) {
   const getNavLinkClass = (path) => {
@@ -8,37 +9,39 @@ export default function Header(props) {
   };
 
   return (
-    <header className="spacing-sm">
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <BrandIcon />
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <BrandIcon />
 
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
-                <Button className="nav-link" href="/" type="link">
-                  Home
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                <Button className="nav-link" href="/browse-by" type="link">
-                  Browse by
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                <Button className="nav-link" href="/stories" type="link">
-                  Stories
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                <Button className="nav-link" href="/agents" type="link">
-                  Agents
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" href="/" type="link">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" href="/browse-by" type="link">
+                    Browse by
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" href="/stories" type="link">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" href="/agents" type="link">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   );
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,46 +1,54 @@
 import React from "react";
 import Button from "elements/Button";
+import Fade from "react-reveal/Fade";
 
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((item, index) => {
-          return (
-            <div
-              key={`mostpicked-${index}`}
-              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
-            >
-              <div className="card card-featured">
-                <div className="tag">
-                  ${item.price}
-                  <span className="font-weight-light"> per {item.unit}</span>
-                </div>
-                <figure className="img-wrapper">
-                  <img
-                    className="img-cover"
-                    src={item.imageUrl}
-                    alt={item.name}
-                  />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button
-                    type="link"
-                    href={`/properties/${item._id}`}
-                    className="stretched-link d-block text-white"
-                  >
-                    {item.name}
-                  </Button>
-                  <span>
-                    {item.city}, {item.country}
-                  </span>
-                </div>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <div
+                key={`mostpicked-${index}`}
+                className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+              >
+                <Fade bottom delay={500 * index}>
+                  <div className="card card-featured">
+                    <div className="tag">
+                      ${item.price}
+                      <span className="font-weight-light">
+                        {" "}
+                        per {item.unit}
+                      </span>
+                    </div>
+                    <figure className="img-wrapper">
+                      <img
+                        className="img-cover"
+                        src={item.imageUrl}
+                        alt={item.name}
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        href={`/properties/${item._id}`}
+                        className="stretched-link d-block text-white"
+                      >
+                        {item.name}
+                      </Button>
+                      <span>
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
+                  </div>
+                </Fade>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </Fade>
     </section>
   );
 }
